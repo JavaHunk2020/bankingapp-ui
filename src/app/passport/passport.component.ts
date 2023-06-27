64094eb7cb7044dac44a4663c16c803d865abf0e
JavaHunk2020 <javahunk2020@gmail.com>
2023-06-28 02:46:54 +0530

    delete passport + shared service + reading parameter from query param

--- src/app/passport/passport.component.ts
+++ src/app/passport/passport.component.ts
@@ -1,5 +1,9 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit,ViewChild ,ElementRef,AfterViewInit} from '@angular/core';
 import { Passport } from '../model/passport.model';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Constant } from 'src/environments/environment';
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-passport',
@@ -11,9 +15,19 @@ export class PassportComponent implements OnInit {
   @Input("passport")
   cpassport={} as Passport;
 
-  constructor() { }
+  constructor(private http:HttpClient,private router:Router,private sharedService:SharedService) { }
 
   ngOnInit(): void {
+    
   }
 
+  deletePassport(pid:number):void {
+     ///passports/{pid}
+      this.http.delete<any>(`${Constant.BASE_URI}/passports/${pid}`)
+      .subscribe((data)=> {
+          
+          this.sharedService.publish("refreshIt");
+          this.cpassport={} as Passport;
+      });
+  }
 }
