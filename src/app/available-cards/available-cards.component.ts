91051ee4d3c6e78d93708035e4b41568b27cb73e
JavaHunk2020 <javahunk2020@gmail.com>
2023-07-11 02:58:24 +0530

    show all the available master cards

--- src/app/available-cards/available-cards.component.ts
+++ src/app/available-cards/available-cards.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Constant } from 'src/environments/environment';
+import { CrediCardType } from '../model/credit.cardtype.model';
 
 @Component({
   selector: 'app-available-cards',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AvailableCardsComponent implements OnInit {
 
-  constructor() { }
+  data:CrediCardType[]={} as Array<CrediCardType>;
+
+   showData:boolean = false;
+
+  constructor(private httpClient : HttpClient) { }
 
   ngOnInit(): void {
+    this.httpClient.get<CrediCardType[]>(`${Constant.BASE_URI}/creditcards/available`).subscribe((data:CrediCardType[])=>{
+        console.log(data)
+        this.data = data;
+        this.data=this.data.map(s=>{
+          s.image=`${Constant.BASE_URI}/creditcards/image?ctid=${s.id}`;
+          return s;
+        })
+        this.showData=true;
+    });
   }
 
 }
---
+++ src/app/model/credit.cardtype.model.ts
@@ -0,0 +1,14 @@
+export interface CrediCardType {
+    id:number;
+	name:string;
+	ccode:string;
+	benefit1:string;
+	benefit2:string;
+	benefit3:string;
+	benefit4:string;
+	joinFee:string;
+	annualFee:string;
+	doe:Date;
+	dom:Date;
+    image:string;
+}
