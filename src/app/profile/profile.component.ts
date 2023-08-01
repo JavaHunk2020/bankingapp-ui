6e8e82966fd7350efedd56fdfd0f17248b5a370f
JavaHunk2020 <javahunk2020@gmail.com>
2023-08-02 02:11:22 +0530

    fix for image upload

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Route, Router } from '@angular/router';
 import { Constant } from 'src/environments/environment';
 
@@ -12,7 +13,7 @@ import { Constant } from 'src/environments/environment';
 export class ProfileComponent implements OnInit {
 
   selectFile:any;
-  
+  cphoto:any;
   myForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.minLength(3)]),
     gender: new FormControl(''),
@@ -21,18 +22,45 @@ export class ProfileComponent implements OnInit {
   });
 
 
-  constructor(private http:HttpClient,private router:Router) { }
+  ngOnInit(): void {
+    let loggedUser=localStorage.getItem("loggedUser") || '';
+    let email = JSON.parse(loggedUser).email;
+    const uri=`${Constant.BASE_URI}/signups/${email}`;
+     this.http.get(uri)
+      .subscribe((signup:any) => {
+        this.myForm.controls['name'].setValue(signup.name);
+        this.myForm.controls['gender'].setValue(signup.gender);
+        this.cphoto= 'data:image/png;base64,' + signup.pphoto;
+        //pphoto
+      });
+  }
+
+
+
+  constructor(private http:HttpClient,private router:Router,private sanitizer: DomSanitizer) { }
 
   onFileChange(event:any) :void {
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
       this.selectFile=file;
+
+      const fileReader = new FileReader()
+    fileReader.readAsDataURL(file)
+
+    fileReader.onload = () => {
+        // You'll get the base64 string here
+        console.log("base64 :", fileReader.result)
+        this.cphoto = this.sanitizer.bypassSecurityTrustUrl(fileReader.result+'');
+    }
+
+    fileReader.onerror = (error) => {
+        console.log("Error reading file :", error)
+    }
+      
     }
   }
 
-  ngOnInit(): void {
-  }
-
+  
   get formControls(){
     return this.myForm.controls;
   }
